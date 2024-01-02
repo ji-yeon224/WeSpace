916bcebac8ed91780ef74e8675d3027637da13f6
ji-yeon224 <69784492+ji-yeon224@users.noreply.github.com>
2024-01-02 16:09:54 +0900

    Design: Set Color Assets

---
+++ SeSACTalk/Utility/Constants/Color.swift
@@ -0,0 +1,29 @@
+//
+//  Color.swift
+//  SeSACTalk
+//
+//  Created by 김지연 on 1/2/24.
+//
+
+import UIKit
+
+extension Constants {
+    
+    static let background = UIColor(named: "Background")
+    static let secondaryBG = UIColor(named: "SecondaryBackground")
+    
+    static let alpha = UIColor(named: "Alpha")
+    static let seperator = UIColor(named: "Seperator")
+    
+    static let basicText = UIColor(named: "BasicText")
+    static let secondaryText = UIColor(named: "SecondaryText")
+    
+    static let inActive = UIColor(named: "Inactive")
+    static let error = UIColor(named: "Error")
+    static let green = UIColor(named: "Green")
+    static let white = UIColor(named: "White")
+    static let black = UIColor(named: "Black")
+    static let gray = UIColor(named: "Gray")
+    
+    
+}
---
+++ SeSACTalk/Utility/Constants/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  SeSACTalk
+//
+//  Created by 김지연 on 1/2/24.
+//
+
+import Foundation
+final class Constants {
+    
+}
