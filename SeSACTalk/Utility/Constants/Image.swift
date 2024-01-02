fe69a37f2b90e561bc027a38a6dae768d85134ca
ji-yeon224 <69784492+ji-yeon224@users.noreply.github.com>
2024-01-02 22:44:30 +0900

    Design: Set Image Assets

---
+++ SeSACTalk/Utility/Constants/Image.swift
@@ -0,0 +1,13 @@
+//
+//  Image.swift
+//  SeSACTalk
+//
+//  Created by 김지연 on 1/2/24.
+//
+
+import UIKit
+
+extension Constants {
+    static let onBoarding = UIImage(named: "OnBoarding")
+    static let xmark = UIImage(named: "Close")
+}
