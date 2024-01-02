ba2b4c8b11db6dbb483907798ea8f32f5ef3ac61
ji-yeon224 <69784492+ji-yeon224@users.noreply.github.com>
2024-01-02 15:46:21 +0900

    Initial Commit

---
+++ SeSACTalk/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSACTalk
+//
+//  Created by 김지연 on 1/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
