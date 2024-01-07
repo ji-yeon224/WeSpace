e542980c1d027cc7bbf1cd4dd8eff0f680f7904a
ji-yeon224 <69784492+ji-yeon224@users.noreply.github.com>
2024-01-08 02:02:36 +0900

    [Refactor] Apply ReactorKit to LoginView KakaoLogin

--- SeSACTalk/Scene/Login/Login/LoginViewController.swift
+++ SeSACTalk/Scene/Login/Login/LoginViewController.swift
@@ -9,12 +9,13 @@ import UIKit
 import RxSwift
 import RxCocoa
 import RxGesture
+import ReactorKit
 
-final class LoginViewController: BaseViewController {
+final class LoginViewController: BaseViewController, View {
     
     private let viewModel = LoginViewModel()
     private let mainView = LoginView()
-    private let disposeBag = DisposeBag()
+    var disposeBag = DisposeBag()
     
     weak var delegate: LoginViewControllerDelegate?
     
@@ -24,14 +25,65 @@ final class LoginViewController: BaseViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        bind()
+//        bind1()
+        self.reactor = LoginReactor()
+        
     }
     
     override func configure() {
         super.configure()
     }
     
-    private func bind() {
+    func bind(reactor: LoginReactor) {
+        bindAction(reactor: reactor)
+        bindState(reactor: reactor)
+    }
+    
+    private func bindAction(reactor: LoginReactor) {
+        
+        mainView.kakaoButton.rx.tap
+            .map { Reactor.Action.kakaoLogin }
+            .bind(to: reactor.action)
+            .disposed(by: disposeBag)
+        
+    }
+    
+    private func bindState(reactor: LoginReactor) {
+        
+        reactor.state
+            .map { $0.kakaoLoginSuccess }
+            .distinctUntilChanged()
+            .map { Reactor.Action.requestKakaoComplete(oauth: $0)}
+            .bind(to: reactor.action)
+            .disposed(by: disposeBag)
+        
+        reactor.state
+            .map { $0.loginSuccess }
+            .distinctUntilChanged()
+            .bind(with: self) { owner, _ in
+                owner.presentInitialView()
+            }
+            .disposed(by: disposeBag)
+        
+        reactor.state
+            .map { $0.msg }
+            .distinctUntilChanged()
+            .bind { value in
+                print(value ?? "error-")
+            }
+            .disposed(by: disposeBag)
+        
+    }
+    
+    private func presentInitialView() {
+        let vc = InitialViewController()
+        let nav = UINavigationController(rootViewController: vc)
+        nav.setupBarAppearance()
+        view.window?.rootViewController = nav
+        view.window?.makeKeyAndVisible()
+    }
+    
+    private func bind1() {
         
         let input = LoginViewModel.Input(kakaoLogin: mainView.kakaoButton.rx.tap)
         let output = viewModel.transform(input: input)
