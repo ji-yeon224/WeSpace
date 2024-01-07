24bcbbea81fb77bf518a5a177eed1a65443ac93c
ji-yeon224 <69784492+ji-yeon224@users.noreply.github.com>
2024-01-07 16:35:12 +0900

    [Feat] Change EmailJoinView Complete View

--- SeSACTalk/Scene/WorkSpace/Initial/InitialViewController.swift
+++ SeSACTalk/Scene/WorkSpace/Initial/InitialViewController.swift
@@ -19,12 +19,23 @@ final class InitialViewController: BaseViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "시작하기"
+        configNav()
     }
     
     override func configure() {
         super.configure()
     }
     
+    private func configNav() {
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: Constants.Image.xmark, style: .plain, target: self, action: #selector(xButtonTapped))
+        navigationItem.leftBarButtonItem?.tintColor = .basicText
+    }
     
+    @objc func xButtonTapped() {
+        let vc = HomeEmptyViewController()
+        let nav = UINavigationController(rootViewController: vc)
+        view.window?.rootViewController = nav
+        view.window?.makeKeyAndVisible()
+    }
     
 }
