558fef6e16a204d5bc27208046a714dc13d248b8
ji-yeon224 <69784492+ji-yeon224@users.noreply.github.com>
2024-02-09 00:38:07 +0900

    Move Directory

---
+++ SeSACTalk/Model/Realm/DM/DmDTO.swift
@@ -0,0 +1,8 @@
+//
+//  DmDTO.swift
+//  SeSACTalk
+//
+//  Created by 김지연 on 2/9/24.
+//
+
+import Foundation
--- SeSACTalk/Model/Realm/ImageDTO.swift
+++ SeSACTalk/Model/Realm/ImageDTO.swift
@@ -11,6 +11,7 @@ import RealmSwift
 final class ImageDTO: Object {
     @Persisted(primaryKey: true) var url: String
     @Persisted(originProperty: "imgItem") var channelInfo: LinkingObjects<ChannelDTO>
+    @Persisted(originProperty: "dmImg") var dmInfo: LinkingObjects<DmDTO>
     
     convenience init(url: String) {
         self.init()
