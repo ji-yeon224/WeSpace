6b749f1372236fb8d2eb67e7d5ccbbce7ea67447
ji-yeon224 <69784492+ji-yeon224@users.noreply.github.com>
2024-02-14 16:27:39 +0900

    [Feat] Set EditProfileView Button Enable

--- SeSACTalk/Scene/Profile/Edit/EditProfileViewController.swift
+++ SeSACTalk/Scene/Profile/Edit/EditProfileViewController.swift
@@ -6,12 +6,17 @@
 //
 
 import UIKit
+import ReactorKit
+import RxCocoa
+import AnyFormatKit
 
 final class EditProfileViewController: BaseViewController {
     
     private let mainView = EditProfileView()
     private var editType: EditProfileType = .editNickname
     private var profileData: ProfileUpdateReqDTO?
+    
+    var disposeBag = DisposeBag()
     
     init(type: EditProfileType, data: ProfileUpdateReqDTO) {
         super.init(nibName: nil, bundle: nil)
@@ -30,7 +35,7 @@ final class EditProfileViewController: BaseViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        bindEvent()
     }
     
     override func configure() {
@@ -40,6 +45,65 @@ final class EditProfileViewController: BaseViewController {
         guard let profileData = profileData else { return }
         mainView.configTextFieldData(type: editType, data: profileData)
         
+    }
+    
+    private func bindEvent() {
+        
+        
+        switch editType {
+        case .editNickname:
+            bindNicknameEvent()
+        case .editPhone:
+            bindPhoneEvent()
+        }
+    }
+    
+    private func bindNicknameEvent() {
+        mainView.editTextfield.rx.text.orEmpty
+            .asDriver()
+            .drive(with: self) { owner, value in
+                let enable = value.isEmpty || value == owner.profileData?.nickName
+                owner.mainView.setButtonEnable(isActive: !enable)
+            }
+            .disposed(by: disposeBag)
+    }
+    
+    private func bindPhoneEvent() {
+        mainView.editTextfield.rx.text.orEmpty
+            .asDriver()
+            .drive(with: self) { owner, value in
+                let empty = value.isEmpty || value == owner.profileData?.phone
+                owner.mainView.setButtonEnable(isActive: !empty)
+            }
+            .disposed(by: disposeBag)
+        
+        mainView.editTextfield.rx.text.orEmpty
+            .asDriver()
+            .map {
+                if "\($0)".lastString.isNumber {
+                    return "\($0)"
+                } else {
+                    let index = "\($0)".index("\($0)".startIndex, offsetBy: $0.count)
+                    return String($0[..<index])
+                }
+            }
+            .drive(with: self) { owner, value in
+                if value.count > 13 {
+                    let index = value.index(value.startIndex, offsetBy: 13)
+                    owner.mainView.editTextfield.text = String(value[..<index])
+                } else {
+                    let characterSet = CharacterSet(charactersIn: value.lastString)
+                    if CharacterSet.decimalDigits.isSuperset(of: characterSet) == false {
+                        owner.mainView.editTextfield.text = owner.mainView.editTextfield.text
+                        
+                    } else {
+                        let formatter = DefaultTextInputFormatter(textPattern: "###-####-####")
+                        let result = formatter.formatInput(currentText: value, range: NSRange(location: value.count-1, length: 1), replacementString: value.lastString)
+                        owner.mainView.editTextfield.text = result.formattedText
+                    }
+                }
+            }
+            .disposed(by: disposeBag)
     }
     
     
